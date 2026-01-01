91faf68a3774988b6a3504734da33c9382da61b0
Martin Romão da Silva <martoooodev@gmail.com>
2026-01-01 19:48:39 -0300

    feat (orderService) : adds orderService's functions; adds more users and orders to the data sample

--- services/orderService.js
+++ services/orderService.js
@@ -1,14 +1,29 @@
 const orders = require('../data/orders');
-console.log(orders);
+// console.log(orders);
 
-function getPaidOrders(orders) {
-
+function getPaidOrders(orders) {    // OK
+    return orders.filter(
+        order => order.paid === true
+    );
 }
 
-function getTotalRevenue(orders) {
+function getTotalRevenue(orders) {  // OK
+    return orders.reduce(
+        (value, order) => {
+            if (order.paid === true) {
+                value += order.total
+            }
 
+            return value;
+        }
+    , 0);
 }
 
-function getOrdersByUser(userId) {
-
+function getOrdersByUser(orders, userId) {  //
+    return orders.filter(
+        order =>
+            order.userId == userId
+    );
 }
+
+console.log(getOrdersByUser(orders, 1));
