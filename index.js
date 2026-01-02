3109de92b8b3ddcdeb07c9729c94b8b0e3093ea7
Martin Romão da Silva <martoooodev@gmail.com>
2026-01-02 08:58:16 -0300

    feat : Imports all functions to index to allow calling them

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// IMPORTAÇÕES ===============================================================
 const users = require('./data/users');
 const orders = require('./data/orders');
+const {
+    getTotalSold,
+    getTotalPaidOrders,
+    getTicketMedio,
+} = require('./report/salesReport');
+const {
+    countUsersPerRole,
+    countActiveUsers,
+} = require('./report/usersReport');
+const {
+    getPaidOrders,
+    getTotalRevenue,
+    getOrdersByUser,
+} = require('./services/orderService');
+const {
+    getActiveUsers,
+    getAdmins,
+    getUsernamesUppercase,
+} = require('./services/userService');
+// IMPORTAÇÕES ===============================================================
+
+console.log(getTotalPaidOrders(orders));
